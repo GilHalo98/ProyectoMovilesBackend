258bd6abbd9862a9904102b8d7cb9ef80d7c7ca8
Diego Gil <diego_gilhalo98@homail.com>
2022-02-23 18:25:27 -0600

    Endpoint para verificar usernames agregado

--- api/routes/UsuarioRoute.js
+++ api/routes/UsuarioRoute.js
@@ -11,6 +11,9 @@ module.exports = (app) => {
     // Registra un usuario en la db.
     router.post("/registrar/", usuario.registrar);
 
+    // Verifica si un nombre de usuario ya existe en la db.
+    router.get("/verificar/username/", usuario.verificarUserName);
+
     // Ruta general de usaurios.
     app.use(process.env.API_URL + "usuario", router);
 };
