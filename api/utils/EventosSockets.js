6ad5bf9be257376a3c5c0f0c09078505e492715d
Diego Gil <diego_gilhalo98@homail.com>
2022-04-02 19:39:39 -0600

    Enum de Eventos para sockets agregados

---
+++ api/utils/EventosSockets.js
@@ -0,0 +1,10 @@
+class EventosSockets {
+    CONEXION = 'connect';
+    DESCONEXION = 'disconnect';
+    MENSAJE_ENVIADO = 'mensaje_enviado';
+    USUARIO_DISPONIBLE = 'cliente_disponible';
+}
+
+module.exports = {
+    EventosSockets
+}
