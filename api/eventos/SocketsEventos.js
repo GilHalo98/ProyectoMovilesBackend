5dad222faac649756e65518e85fcb8b13be598fe
Diego Gil <diego_gilhalo98@homail.com>
2022-04-02 19:43:26 -0600

    Agregado Enum para eventos de sockets

--- api/eventos/SocketsEventos.js
+++ api/eventos/SocketsEventos.js
@@ -1,14 +1,26 @@
+const Eventos = require("../utils/EventosSockets");
+const EVENTOS = new Eventos.EventosSockets();
+
+const diccionarioUsuarios = {}
+
 module.exports = (io) => {
-  io.on('connect', (socket) => {
+  io.on(EVENTOS.CONEXION, (socket) => {
     console.log('a user connected');
 
-    socket.on('disconnect', () => {
+    socket.on(EVENTOS.DESCONEXION, () => {
       console.log('user disconnected');
     });
 
-    socket.on('chat message', (msg) => {
+    socket.on(EVENTOS.MENSAJE_ENVIADO, (msg) => {
       console.log('message: ' + msg);
-      socket.broadcast.emit('chat message', msg);
+      socket.broadcast.emit(EVENTOS.MENSAJE_ENVIADO, msg);
+    });
+
+    socket.on(EVENTOS.USUARIO_DISPONIBLE, (cliente) => {
+      console.log('cliente conectado: ' + cliente);
+      diccionarioUsuarios[cliente] = socket.id;
+      console.log(diccionarioUsuarios);
+      socket.broadcast.emit(EVENTOS.USUARIO_DISPONIBLE, cliente);
     });
   });
 };
