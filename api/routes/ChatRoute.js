dd24775aa059c0ea82b9d6ddbe0c5919c3f88ab5
Diego Gil <diego_gilhalo98@homail.com>
2022-04-02 19:40:51 -0600

    ruta de sala de chat agregada

---
+++ api/routes/ChatRoute.js
@@ -0,0 +1,17 @@
+module.exports = (app) => {
+    // Controlador del endpoint.
+    const chat = require("../controladores/ControladorChat.js");
+
+    // Se importan las variables del ambiente.
+    require("dotenv").config();
+
+    // Enrutador de funciones.
+    var router = require("express").Router();
+
+    // router.get("/chat/userToken=:token", chat.vistaChat);
+
+    router.get("/chat/", chat.vistaChat);
+
+    // Ruta general de usaurios.
+    app.use(process.env.API_URL + "vistas", router);
+};
