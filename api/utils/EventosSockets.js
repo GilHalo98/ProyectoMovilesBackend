5fa3a3e6e26ccc9b7bb1a8f1139cdf447478666f
Diego Gil <diego_gilhalo98@homail.com>
2022-05-09 14:33:36 -0500

    Evento de mensaje privado y paso de token por header agregado

--- api/utils/EventosSockets.js
+++ api/utils/EventosSockets.js
@@ -2,11 +2,13 @@ class EventosSockets {
     CONEXION = 'connect';
     DESCONEXION = 'disconnect';
 
-    MENSAJE_ENVIADO = 'mensaje_enviado';
     MENSAJE_ENVIADO_PRIVADO = 'mensaje_privado';
 
     CLIENTE_DISPONIBLE = 'cliente_disponible';
     CLIENTE_TERMINADO = 'cliente_terminado';
+
+    CONSULTA_CLIENTES = 'consulta_clientes';
+    CONSULTA_CONTACTOS = 'consulta_contactos';
 }
 
 module.exports = {
